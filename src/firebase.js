03c0e0ac4a65eac36c48b1769d02337f8fa48b2c
Florian Hitz <web.florianhitz@gmail.com>
2022-02-28 16:27:49 +0100

    finished automated deployment and firebase appcheck

--- src/firebase.js
+++ src/firebase.js
@@ -22,7 +22,10 @@ const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 const auth = getAuth();
 
-window.self.FIREBASE_APPCHECK_DEBUG_TOKEN = process.env.APP_CHECK_DEBUG_TOKEN_FROM_CI;
+if (process.env.NODE_ENV === 'development') {
+  window.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
+}
+
 // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
 // key is the counterpart to the secret key you set in the Firebase console.
 const appCheck = initializeAppCheck(app, {
