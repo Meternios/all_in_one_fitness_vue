ece6d06fcee05c88706b09f7b22c8f4dad2bee6a
Florian Hitz <web.florianhitz@gmail.com>
2022-03-26 00:12:03 +0100

    WIP on clousmessaging firebase

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,6 +1,16 @@
-// Import and configure the Firebase SDK
-// These scripts are made available when the app is served or deployed on Firebase Hosting
-// If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
-importScripts('/__/firebase/9.2.0/firebase-app-compat.js');
-importScripts('/__/firebase/9.2.0/firebase-messaging-compat.js');
-importScripts('/__/firebase/init.js');
+importScripts('/__/firebase/9.6.10/firebase-app-compat.js');
+importScripts('/__/firebase/9.6.10/firebase-messaging-compat.js');
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBnvRQFRJq2r_ddhU2HUcz1QwVRB3GOs7o',
+  authDomain: 'all-in-one-fitness-b99d0.firebaseapp.com',
+  databaseURL: 'https://all-in-one-fitness-b99d0-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'all-in-one-fitness-b99d0',
+  storageBucket: 'all-in-one-fitness-b99d0.appspot.com',
+  messagingSenderId: '397087168802',
+  appId: '1:397087168802:web:ba8de390ea49b8ec78ed7b',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
